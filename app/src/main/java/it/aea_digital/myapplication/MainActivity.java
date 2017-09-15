0bbd4a0380d3d893bc7bd0096ad2ad212544c1c1
Andrea Gargarone <CapacitorGreen123>
2017-09-15 16:13:35 +0200

    Eliminazione import inutili

--- app/src/main/java/it/aea_digital/myapplication/MainActivity.java
+++ app/src/main/java/it/aea_digital/myapplication/MainActivity.java
@@ -1,26 +1,11 @@
 package it.aea_digital.myapplication;
-import android.os.AsyncTask;
+
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import Connection.Connection;
-import Connection.HttpPostActivity;
 
 public class MainActivity extends AppCompatActivity {
 
